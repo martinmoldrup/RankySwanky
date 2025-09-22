fdcb78850d589b0472634a37f9039e997d9e947b
Martin Møldrup <14809761+martinmoldrup@users.noreply.github.com>
2025-09-22 11:57:07 +0200

    formatting updates

--- rankyswanky/adapters/persistence/pydantic_caching.py
+++ rankyswanky/adapters/persistence/pydantic_caching.py
@@ -1,11 +1,14 @@
 """
-Reads pydantic models and stores them in SQLite database using SQLModel.
+Read pydantic models and stores them in SQLite database using SQLModel.
+
 Functions for caching and retrieving models from the database.
 """
-from typing import Dict, Any, Type
-from sqlmodel import SQLModel, Field, create_engine, Session
-from rankyswanky.adapters.persistence.caching_models import Document
+
 import pathlib
+from rankyswanky.adapters.persistence.caching_models import Document
+from sqlmodel import Field, Session, SQLModel, create_engine
+from typing import Any, Dict, Type
+
 
 def clear_database(db_path: str = "documents.db") -> None:
     """Deletes the db file."""
@@ -15,30 +18,36 @@ def clear_database(db_path: str = "documents.db") -> None:
 
 
 def create_schema(db_path: str = "documents.db") -> None:
-    """Creates the documents table schema in the SQLite database."""
+    """Create the documents table schema in the SQLite database."""
     engine = create_engine(f"sqlite:///{db_path}")
     SQLModel.metadata.create_all(engine)
 
+
 def save_document_to_db(document: Document, db_path: str = "documents.db") -> None:
-    """Saves a Document instance to the SQLite database."""
+    """Save a Document instance to the SQLite database."""
     engine = create_engine(f"sqlite:///{db_path}")
     with Session(engine) as session:
         session.merge(document)
         session.commit()
 
-def get_sqlmodel_by_primary_key(model: Type[SQLModel], primary_key_value: str, db_path: str = "documents.db") -> SQLModel | None:
-    """Retrieves a SQLModel instance by its ID from the SQLite database."""
+
+def get_sqlmodel_by_primary_key(
+    model: Type[SQLModel], primary_key_value: str, db_path: str = "documents.db"
+) -> SQLModel | None:
+    """Retrieve a SQLModel instance by its ID from the SQLite database."""
     engine = create_engine(f"sqlite:///{db_path}")
     with Session(engine) as session:
         return session.get(model, primary_key_value)
 
+
 def save_sqlmodels_to_db(models: list[SQLModel], db_path: str = "documents.db") -> None:
-    """Saves a list of SQLModel instances to the SQLite database."""
+    """Save a list of SQLModel instances to the SQLite database."""
     engine = create_engine(f"sqlite:///{db_path}")
     with Session(engine) as session:
         for model in models:
             session.merge(model)
         session.commit()
+
 
 if __name__ == "__main__":
     dummy_document: Document = Document(
@@ -46,7 +55,7 @@ if __name__ == "__main__":
         content="This is a test document.",
         embedding_vector=[0.1, 0.2, 0.3],
         metadata={"author": "John Doe"},
-        hash="abc123"
+        hash="abc123",
     )
     clear_database()
     create_schema()
--- rankyswanky/adapters/persistence/repositories_sqllite.py
+++ rankyswanky/adapters/persistence/repositories_sqllite.py
@@ -12,6 +12,7 @@ class QuestionWithRewritesAndCorrectnessPropsRepositorySQLite(QuestionWithRewrit
     def get_by_question_and_perspective(
         self, question: str, perspective: str
     ) -> Optional[QuestionWithRewritesAndCorrectnessProps]:
+        """Retrieve a QuestionWithRewritesAndCorrectnessProps by question and perspective."""
         persisted_model = pydantic_caching.get_sqlmodel_by_primary_key(
             model=GenAndEvaluateQuestionParameters,
             primary_key_value=mapper_domain_to_caching_models.default_gen_eval_id_strategy(
@@ -29,5 +30,6 @@ class QuestionWithRewritesAndCorrectnessPropsRepositorySQLite(QuestionWithRewrit
         )
 
     def save(self, params: QuestionWithRewritesAndCorrectnessProps) -> None:
+        """Save a QuestionWithRewritesAndCorrectnessProps to the database."""
         persistence_obj = mapper_domain_to_caching_models.map_combined_output_to_gen_and_evaluate_params(params)
         pydantic_caching.save_sqlmodels_to_db([persistence_obj])
